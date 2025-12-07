0abddd90e5a8aa305df0f39b1aae843ca4fec60c
Felipe Farias <felipefarias97@icloud.com>
2025-12-07 04:27:38 -0300

    :construction: Adicionado remoção de ingredientes da "Sua lista"

--- src/components/ConteudoPrincipal.vue
+++ src/components/ConteudoPrincipal.vue
@@ -14,6 +14,9 @@ export default {
     methods: {
         adicionarIngrediente(ingrediente: string) {
             this.ingredientes.push(ingrediente)
+        },
+        removerIngrediente(ingrediente: string) {
+            this.ingredientes = this.ingredientes.filter(iLista => ingrediente !== iLista);
         }
     }
 
@@ -23,7 +26,8 @@ export default {
 <template>
     <main class="conteudo-principal">
         <SuaLista :ingredientes="ingredientes" />
-        <SelecionarIngredientes @adicionar-ingrediente="adicionarIngrediente"/>
+        <SelecionarIngredientes @adicionar-ingrediente="adicionarIngrediente"
+            @remover-ingrediente="removerIngrediente" />
     </main>
 </template>
 
